b010551dd34607e8206286dda2ca50316ffbffbd
SongkranUthathong <songkran.u.github@gmail.com>
2023-08-08 01:21:45 +0700

    commit

--- Object/ChartObject.py
+++ Object/ChartObject.py
@@ -11,63 +11,94 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from Object.Exception import *
 from rtde_receive import RTDEReceiveInterface as RTDEReceive
 
-
+MAX = 200
+MS = 50
 class CustomThread(QThread):
 
-    signal = pyqtSignal(np.ndarray)
+    signal = pyqtSignal(list)
 
     def __init__(self, parent=None):
         super(CustomThread, self).__init__(parent)
         # self.rtde_r = rtde_receive.RTDEReceiveInterface("192.168.47.128")
         self.rtde_r = RTDEReceive("192.168.47.128")
-        self.MAX = 5000
-        self.FX = [0] * self.MAX
+        self.count = 1
+        self.FX = [0] * MAX
+        self.FY = [0] * MAX
+        self.FZ = [0] * MAX
+        self.TX = [0] * MAX
+        self.TY = [0] * MAX
+        self.TZ = [0] * MAX
     def run(self):
         # This method will be executed in a separate thread
         while True:
             actual_q = self.rtde_r.getActualQ()
-            for i in range(self.MAX):
-                if i < self.MAX - 1:
+            r2d = [180/np.pi]*6
+            actual_q = [actual_q[i] * r2d[i] for i in range(len(actual_q))]
+            for i in range(MAX):
+                if i < MAX - 1:
                     self.FX[i] = self.FX[i+1]
+                    self.FY[i] = self.FY[i+1]
+                    self.FZ[i] = self.FZ[i+1]
+                    self.TX[i] = self.TX[i+1]
+                    self.TY[i] = self.TY[i+1]
+                    self.TZ[i] = self.TZ[i+1]
                 else:
                     self.FX[i] = actual_q[0]
-                # print(i)
+                    self.FY[i] = actual_q[1]
+                    self.FZ[i] = actual_q[2]
+                    self.TX[i] = actual_q[3]
+                    self.TY[i] = actual_q[4]
+                    self.TZ[i] = actual_q[5]
+                self.count +=1
+            self.signal.emit([np.array(self.FX),np.array(self.FY),np.array(self.FZ),np.array(self.TX),np.array(self.TY),np.array(self.TZ)])
+            time.sleep(1/MS)  # Simulate a time-consuming operation
             
-            time.sleep(1/100)  # Simulate a time-consuming operation
-            self.signal.emit(np.array(self.FX))
-        # self.signal.emit("Task completed!")
-        # msgPrint("Task completed!")
-
 
 class PlotProfile(QWidget):
     def __init__(self, parent=None):
         super(PlotProfile, self).__init__(parent)
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvas(self.figure)
-        self.x = np.linspace(0, int(5000/100),5000)
-        self.y = np.linspace(0,1,5000)
-        # Create the plot line
-        self.plot_line, = self.ax.plot(self.x, self.y)
-        self.ax.draw_artist(self.plot_line)
+        self.sampling_interval = 1 / MS
+        self.time_in_sec = np.arange(MAX) * self.sampling_interval
+        # self.x = np.linspace(0, 1/MS,num=MAX)
+        # self.y = np.linspace(0,0,MAX)
 
 class ForceTorqeChart(QMainWindow):
     def __init__(self,MainWindows):
         super().__init__()
 
-        # Force Chart
-        self.j = 0
-
         self.MainWindows = MainWindows
 
+        # SET Force & Torque Chart
         self.animated_widget1 = PlotProfile(self)
+        self.animated_widget2 = PlotProfile(self)
+        self.animated_widget1.ax.set_xlabel("Time (s)")
+        self.animated_widget1.ax.set_ylabel("N")
+        self.animated_widget1.ax.set_title('FORCE')
+        self.animated_widget2.ax.set_xlabel("Time (s)")
+        self.animated_widget2.ax.set_ylabel("N*m")
+        self.animated_widget2.ax.set_title('TORQUE')
+        self.ChartFX = self.animated_widget1.ax.plot(0,0,c='r',label='FX :{} N'.format(10))[0]
+        self.ChartFY = self.animated_widget1.ax.plot(0,0,c='g',label='FY :{} N'.format(10))[0]
+        self.ChartFZ = self.animated_widget1.ax.plot(0,0,c='b',label='FZ :{} N'.format(10))[0]
 
+        self.ChartTX = self.animated_widget2.ax.plot(0,0,c='r',label='TX :{} N'.format(10))[0]
+        self.ChartTY = self.animated_widget2.ax.plot(0,0,c='g',label='TY :{} N'.format(10))[0]
+        self.ChartTZ = self.animated_widget2.ax.plot(0,0,c='b',label='TZ :{} N'.format(10))[0]
+        self.animated_widget1.ax.legend()
         layout1 = QVBoxLayout()
         layout1.addWidget(self.animated_widget1.canvas)
-        # # layout2 = QVBoxLayout()
-        # # layout2.addWidget(animated_widget2)
-        # # self.setCentralWidget(animated_widget)
-        # # self.MainWindows.setCentralWidget(animated_widget)
+        layout2 = QVBoxLayout()
+        layout2.addWidget(self.animated_widget2.canvas)
+
+
+
+
+        # Set Lay out to Widget
         self.MainWindows.chart_Force.setLayout(layout1)
+        self.MainWindows.chart_Torque.setLayout(layout2)
+
 
     def start_thred(self):
         try:
@@ -85,70 +116,56 @@ class ForceTorqeChart(QMainWindow):
 
     def on_thread_signal(self, FTSensor):
         try:
-            self.animated_widget1.plot_line.set_ydata(FTSensor)
+            # Set Data for FX
+            self.ChartFX.set_ydata(FTSensor[0])
+            self.ChartFX.set_xdata(self.animated_widget1.time_in_sec)
+            # Set Data for FY
+            self.ChartFY.set_ydata(FTSensor[1])
+            self.ChartFY.set_xdata(self.animated_widget1.time_in_sec)
+            # # Set Data for FZ
+            self.ChartFZ.set_ydata(FTSensor[2])
+            self.ChartFZ.set_xdata(self.animated_widget1.time_in_sec)
+            # Set Data for TX
+            self.ChartTX.set_ydata(FTSensor[3])
+            self.ChartTX.set_xdata(self.animated_widget2.time_in_sec)
+            # Set Data for TY
+            self.ChartTY.set_ydata(FTSensor[4])
+            self.ChartTY.set_xdata(self.animated_widget2.time_in_sec)
+            # # Set Data for TZ
+            self.ChartTZ.set_ydata(FTSensor[5])
+            self.ChartTZ.set_xdata(self.animated_widget2.time_in_sec)
 
-            self.animated_widget1.ax.draw_artist(self.animated_widget1.plot_line)
+            # UPDATE CHART
             self.animated_widget1.canvas.blit(self.animated_widget1.figure.bbox)
             self.animated_widget1.canvas.flush_events()
             self.animated_widget1.canvas.draw()
-            self.j += 1
+
+            self.animated_widget2.canvas.blit(self.animated_widget2.figure.bbox)
+            self.animated_widget2.canvas.flush_events()
+            self.animated_widget2.canvas.draw()
+
+            # msgPrint(FTSensor)
         except Exception as e:
             msgPrint(str(e))
 
     def addjust_Plort(self):
         try:
-            pass
-            scale_x = self.MainWindows.scaleX.value() / 100
-            scale_y = self.MainWindows.scaleYP.value() / 100
+            
+            scale_x = self.MainWindows.scaleX.value()
+            scale_yp = self.MainWindows.scaleYP.value()
+            scale_yn = self.MainWindows.scaleYN.value()
             # Generate sample data
-            x = np.linspace(self.MainWindows.scaleYN.value(), 100*scale_x, 5000)
-            y = np.sin(x)
+
 
             # Update the plot with the scaled data
-            self.animated_widget1.ax.clear()
-            self.animated_widget1.ax.plot(x, y * scale_y)
-            self.animated_widget1.ax.set_xlabel("X Axis")
-            self.animated_widget1.ax.set_ylabel("Y Axis")
-
+            self.animated_widget1.ax.set_xlim(0,scale_x/10)
+            self.animated_widget1.ax.set_ylim((scale_yn/10)*-1,scale_yp/10)
+            self.animated_widget2.ax.set_xlim(0,scale_x/10)
+            self.animated_widget2.ax.set_ylim((scale_yn/10)*-1,scale_yp/10)
             # Redraw the plot
-            self.animated_widget1.canvas.draw()
+            # self.animated_widget1.canvas.draw()
+
             
-        except Exception as e:
-            msgPrint(str(e))
-
-    def scalueX_Adjust(self):
-        try:
-            __xScale = self.MainWindows.scaleX.value()
-            msgPrint(self.MainWindows.scaleX.value())
-            self.animated_widget1.x = np.linspace(0, int(__xScale/100),__xScale)
-            self.animated_widget1.y = np.linspace(0,1,__xScale)
-
-            print(self.animated_widget1.x)
-
-            # self.animated_widget1.plot_line, = self.animated_widget1.ax.plot(self.animated_widget1.x, self.animated_widget1.y)
-
-            # self.animated_widget1.plot_line.set_ydata(self.animated_widget1.y)
-            self.animated_widget1.plot_line, = self.animated_widget1.ax.plot(self.animated_widget1.x, self.animated_widget1.y)
-
-            self.animated_widget1.ax.draw_artist(self.animated_widget1.plot_line)
-            self.animated_widget1.canvas.blit(self.animated_widget1.figure.bbox)
-            self.animated_widget1.canvas.flush_events()
-            self.animated_widget1.canvas.draw()
-            pass
-        except Exception as e:
-            msgPrint(str(e))
-    
-    def scalueYP_Adjust(self):
-        try:
-            msgPrint(self.MainWindows.scaleYP.value())
-            pass
-        except Exception as e:
-            msgPrint(str(e))
-
-    def scalueYN_Adjust(self):
-        try:
-            msgPrint(self.MainWindows.scaleYN.value())
-            pass
         except Exception as e:
             msgPrint(str(e))
 
